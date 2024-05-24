060aca1a7d59d0a3a79f129547fbde895bb1a144
haorao <haoran@izhimu.cn>
2024-05-24 18:02:13 +0800

    feat: 新增safe适配

--- packages-mobile/seas-security-mobile-view/src/view/TestPage.vue
+++ packages-mobile/seas-security-mobile-view/src/view/TestPage.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-import { Field as VanField } from "vant";
+import {
+  NavBar as VanNavBar,
+  Field as VanField,
+  Sticky as VanSticky,
+  Button as VanButton,
+} from "vant";
+import { useRect } from "@vant/use";
 import { onMounted } from "vue";
 import { current } from "../request/user";
 import { useAuthStore } from "../store";
@@ -14,7 +20,16 @@ onMounted(() => {
 </script>
 
 <template>
-  <van-field v-model="authStore.token" label="Token" placeholder="Load Token" />
+  <van-sticky>
+    <van-nav-bar safe-area-inset-top />
+  </van-sticky>
+  <van-field
+    v-for="i in 50"
+    :key="i"
+    v-model="authStore.token"
+    label="Token"
+    placeholder="Load Token"
+  />
 </template>
 
 <style scoped></style>
