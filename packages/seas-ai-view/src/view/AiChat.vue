4327fcf835edd366e3708755357691fc07b2b8c7
haorao <haoran@izhimu.cn>
2024-08-09 17:30:51 +0800

    feat: 新增SSE demo

--- packages/seas-ai-view/src/view/AiChat.vue
+++ packages/seas-ai-view/src/view/AiChat.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import { useUserStore } from "@izhimu/seas-security-view";
 import ChatWindow from "../components/ChatWindow.vue";
+
+const userStore = useUserStore();
+
+const source = new EventSource(
+  `/api/sse?X-Auth-Token=${userStore.current.token}`,
+);
+source.onmessage = (event) => {
+  console.log(event);
+};
 </script>
 
 <template>
